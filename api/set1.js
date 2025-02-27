d25eca9ff2bb666f0909e69d38225ce282d31b88
Kdzira5 <kdzira5@gmail.com>
2025-02-27 04:17:42 +0300

    Update set1.js

--- api/set1.js
+++ api/set1.js
@@ -1,31 +1,19 @@
 // /api/set1.js
+const server=require('../server'); // Импортируем сервер с функциями setData и getData
 
-const server = require('../server'); // Импортируем переменную и функции из server.js
+module.exports=(req,res)=>{
+  try{
+    const{newData}=req.body; // Получаем новые данные из тела запроса
 
-module.exports = (req, res) => {
-  try {
-    const data=server.storedData;//Получаем данные из глобальной переменно
-    server.setData(newData);// Устанавливаем новые данные с помощью setData
-
-    // Проверяем, что данные существуют
-    if(!data){
-      return res.status(200).json({ message: 'Данные не найдены' });
+    if(!newData){// Проверка, что newData передано в запросе
+      return res.status(400).json({ message:'Данные не переданы'});
     }
+    server.setData(newData);//Сохраняем полученные данные с помощью setData
 
-    // Логика обработки GET-запроса
-    const responseData = {
-      message: 'Информация о моде',
-      modDetails: {
-        message: 'Данные успешно получены',
-        storedData: data,
-      },
-    };
-
-    // Отправляем данные в виде JSON-ответа
-    return res.status(200).json(responseData);
-  } catch (error) {
-    // Обработка ошибок
-    console.error('Ошибка на сервере:', error);
-    return res.status(200).json({ message: 'Ошибка при обработке запроса' });
+    // Отправляем успешный ответ
+    return res.status(200).json({message:'Данные успешно обновлены!'});
+  }catch(error){
+    console.error('Ошибка при обновлении данных:',error);
+    return res.status(500).json({message:'Ошибка при обновлении данных'});
   }
 };
