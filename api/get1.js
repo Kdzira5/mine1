fe79ba9e1a1a45dc4d1dc021fdd5da0968cc22f0
Kdzira5 <kdzira5@gmail.com>
2025-02-27 03:46:04 +0300

    Update get1.js

--- api/get1.js
+++ api/get1.js
@@ -9,7 +9,7 @@ module.exports = (req, res) => {
 
     // Проверяем, что данные существуют
     if (!data) {
-      return res.status(500).json({ message: 'Данные не найдены' });
+      return res.status(200).json({ message: 'Данные не найдены' });
     }
 
     // Логика обработки GET-запроса
@@ -26,6 +26,6 @@ module.exports = (req, res) => {
   } catch (error) {
     // Обработка ошибок
     console.error('Ошибка на сервере:', error);
-    return res.status(500).json({ message: 'Ошибка при обработке запроса' });
+    return res.status(200).json({ message: 'Ошибка при обработке запроса' });
   }
 };
