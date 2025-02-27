57714ccb9edefbdd8fb147beec8d0fe864e009bb
Kdzira5 <kdzira5@gmail.com>
2025-02-27 03:40:45 +0300

    Update get1.js

--- api/get1.js
+++ api/get1.js
@@ -1,14 +1,14 @@
 // /api/getData.js
 
+const server=require('./server');// Импортируем переменную и функции из server.js
+
 module.exports = (req, res) => {
+  const data=server.storedData;//Получаем данные из глобальной переменной 
+  
   // Логика обработки GET-запроса
   const responseData = {
     message: 'Информация о моде',
-    modDetails: {
-      name: 'Minecraft Mod',
-      description: 'Мод для Minecraft, добавляющий новые возможности!',
-      version: '1.0.0',
-    },
+    modDetails: { message:'Данные успешно получены',storedData:data},
   };
 
   // Отправляем данные в виде JSON-ответа
