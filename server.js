6cecb67518a88c44a47fd5c3b0357f16b739f003
Kdzira5 <kdzira5@gmail.com>
2025-02-27 03:08:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,20 +5,9 @@ const app = express();
 // Для парсинга JSON запросов
 app.use(bodyParser.json());
 
-// Обработка POST запроса (отправка данных)
-app.post('/api/sendData', (req, res) => {
-    const data = req.body;
-    console.log("Полученные данные:", data);
 
-    // Отправить ответ
-    res.status(200).json({ message: 'Данные успешно получены!' });
-});
 
-// Обработка GET запроса (получение данных)
-app.get('/api/getData', (req, res) => {
-    // Здесь можно вернуть какие-то данные для мода
-    res.status(200).json({ message: 'Информация о е' });
-});
+
 
 // Запуск сервера
 const PORT = process.env.PORT || 3000;
