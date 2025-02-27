19c9e8166ead5b412301e99e0472f696d7f80591
Kdzira5 <kdzira5@gmail.com>
2025-02-27 03:44:44 +0300

    Update get1.js

--- api/get1.js
+++ api/get1.js
@@ -1,16 +1,31 @@
-// /api/getData.js
+// /api/get1.js
 
-const server=require('./server');// Импортируем переменную и функции из server.js
+const server = require('./server'); // Импортируем переменную и функции из server.js
 
 module.exports = (req, res) => {
-  const data=server.storedData;//Получаем данные из глобальной переменной 
-  
-  // Логика обработки GET-запроса
-  const responseData = {
-    message: 'Информация о моде',
-    modDetails: { message:'Данные успешно получены',storedData:data},
-  };
+  try {
+    // Получаем данные из глобальной переменной
+    const data = server.storedData;
 
-  // Отправляем данные в виде JSON-ответа
-  res.status(200).json(responseData);
+    // Проверяем, что данные существуют
+    if (!data) {
+      return res.status(500).json({ message: 'Данные не найдены' });
+    }
+
+    // Логика обработки GET-запроса
+    const responseData = {
+      message: 'Информация о моде',
+      modDetails: {
+        message: 'Данные успешно получены',
+        storedData: data,
+      },
+    };
+
+    // Отправляем данные в виде JSON-ответа
+    return res.status(200).json(responseData);
+  } catch (error) {
+    // Обработка ошибок
+    console.error('Ошибка на сервере:', error);
+    return res.status(500).json({ message: 'Ошибка при обработке запроса' });
+  }
 };
