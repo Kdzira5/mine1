9e177f0c9d60424389d93cd346de11d5e209f5a3
Kdzira5 <kdzira5@gmail.com>
2025-02-27 03:30:21 +0300

    Update get1.js

--- api/get1.js
+++ api/get1.js
@@ -1,12 +1,15 @@
 // /api/get1.js
 
 // Импортируем переменную и функции из server.js
-const server = require('./server');
+const server=require('./server');
 
-const mongoose = require('mongoose');
+const mongoose=require('mongoose');
 
-module.exports = (req, res) => {
+module.exports=(req,res)=>{
 
+  //Если вы хотите использовать данные из глобальной переменной:
+  const data=server.storedData;//Получаем данные из глобальной переменной
   
-
+  //Отправляем данные в ответе
+  res.status(200).json({ message:'Данные успешно получены',storedData:data});
 };
