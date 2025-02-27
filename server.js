938997a173b1dffaf1966767622d05441da8a1a1
Kdzira5 <kdzira5@gmail.com>
2025-02-27 03:27:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ app.use(bodyParser.json());
 // Создаем глобальную переменную для хранения данных
 let storedData = "Initial data";
 
+// Экспортируем переменную для использования в других файлах
+module.exports.storedData = storedData;
+
+// Экспортируем функцию для изменения данных
+module.exports.setData = (newData) => {
+  storedData = newData;
+};
+
 
 
 // Запуск сервера
